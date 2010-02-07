3bf9a038c58f5f6a5d673f2aecb87922dcb02d59
Olivier Verdier <olivier.verdier@gmail.com>
2010-02-07 11:59:17 +0100

    extended the `setup.py` file

--- setup.py
+++ setup.py
@@ -5,7 +5,20 @@ from distutils.core import setup
 
 setup(
 	name = 'pydflatex',
+	description = 'LaTeX wrapper',
 	author='Olivier Verdier',
+	license = 'GPL',
+	keywords = ['LaTeX', 'pdflatex', 'parser'],
+	
 	scripts=['bin/pydflatex'],
 	packages=['pydflatex'],
+	classifiers = [
+	'Development Status :: 5 - Production/Stable',
+	'Environment :: Console',
+	'Intended Audience :: Science/Research',
+	'License :: OSI Approved :: GNU General Public License (GPL)',
+	'Operating System :: OS Independent',
+	'Programming Language :: Python',
+	'Topic :: Text Processing :: Markup :: LaTeX'],
+	zip_safe = False,
 	)
