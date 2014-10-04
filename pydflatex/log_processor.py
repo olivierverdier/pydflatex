42149c0d8129a86a33416b9be366a56e535af2a0
Olivier Verdier <olivier.verdier@gmail.com>
2014-10-04 13:03:13 +0200

    more refactoring into smaller methods

--- pydflatex/log_processor.py
+++ pydflatex/log_processor.py
@@ -44,25 +44,34 @@ class LogProcessor(Processor):
 		error = self.process_parser(parser)
 		return error
 
-	def process_parser(self, parser):
-		"""
-		Process information from the parser and print out the gist of it.
-		"""
-		for box in parser.get_boxes():
+	def process_boxes(self, boxes):
+		for box in boxes:
 			has_occ = box['text'].find(r' has occurred while \output is active')
 			if has_occ != -1:
 				box['text'] = box['text'][:has_occ]
 			if not self.options['suppress_box_warning']:
 				self.logger.box_warning(box)
-		for ref in parser.get_references():
+
+	def process_references(self, references):
+		for ref in references:
 			self.logger.ref_warning(ref)
-		for warning in parser.get_warnings():
+
+	def process_warnings(self, warnings):
+		for warning in warnings:
 			# following should be filtered via the loggers filter!
 			if warning.get('pkg') == 'hyperref' and warning['text'].find('Token') != -1:
 				continue # I hate those hyperref warning
 			if warning.get('text') == r'Command \centerline is TeX.  Use \centering or center environment instead.':
 				continue # warning from the nag package
 			self.logger.latex_warning(warning)
+
+	def process_parser(self, parser):
+		"""
+		Process information from the parser and print out the gist of it.
+		"""
+		self.process_boxes(parser.get_boxes())
+		self.process_references(parser.get_references())
+		self.process_warnings(parser.get_warnings())
 		errors = list(parser.get_errors())
 		if errors:
 			for error in errors:
